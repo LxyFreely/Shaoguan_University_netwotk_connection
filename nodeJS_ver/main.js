c2c7986161dde4aa435b646a5584aa81c7b43617
ZX_RAY <108106842+ZX-RAY1124@users.noreply.github.com>
2026-06-01 16:11:41 +0800

    修改了前端界面，优化了请求，现已全面支持手动和自动上下线

--- nodeJS_ver/main.js
+++ nodeJS_ver/main.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const path = require('path');
 const iconPath = path.join(__dirname, 'icon.png');
-const {app, BrowserWindow, Tray, Menu} = require('electron')
+const {app, BrowserWindow, Tray, Menu, Notification} = require('electron')
 const server = require('./server/server');
 const fs = require('fs');
-const events = require("node:events");
 
+require("node:events");
 const post_data = {
     'userid':'',
     'passwd':'',
@@ -28,12 +28,15 @@ const post_data = {
     'sendFttrNotice':'0'
 };
 
+let time_index = 0;
+let scheduleInterval = null;
+let waiting = false;
 
 
 
+function make_request(login) {
+    waiting = true;
 
-
-function make_request() {
     const option = {
         host: '2.2.2.2',
         path: '/',
@@ -49,7 +52,9 @@ function make_request() {
         })
         res.on('end', () => {
             console.log('>> Succeed in getting url')
-            handle_request(res.statusCode,data);
+            let location = res.headers.location;
+
+            handle_request(res.statusCode,data,login,location);
         })
         res.on('error', (err) => {
             console.error(`>> Facing error in getting data, please reported to developer: ${err}`)
@@ -68,40 +73,60 @@ function make_request() {
 }
 
 
-async function handle_request(status_code, data){
-    if(status_code === 302){
-        console.log(`>> You are already login! [code: ${status_code}]`)
-        let local_temp;
-        local_temp = await new Promise((resolve, reject) => {
-            fs.readFile(path.join(__dirname, 'request_param.json'), 'utf-8', (err, data) => {
-                resolve(data)
-                if(err){
-                    console.warn('>> Load file from local failed, this may not affect the program, please add file "request_param.json", below the dir')
-                    reject(err)
-                }
+async function handle_request(status_code, data, login, loc) {
+    if(status_code === 302) {
+        if(!loc) {
+            let local_temp;
+            local_temp = await new Promise((resolve, reject) => {
+                fs.readFile(path.join(__dirname, 'request_param.json'), 'utf-8', (err, data) => {
+                    resolve(data)
+                    if (err) {
+                        console.warn('>> Load file from local failed, this may not affect the program, please add file "request_param.json", below the dir')
+                        reject(err)
+                    }
+                });
             });
-        });
-
-        local_temp = JSON.parse(local_temp);
-
-        post_data.userid = '25122021076';
-        post_data.passwd = '17881952248'
-        post_data.wlanuserip = local_temp.portalForm.wlanuserip
-        post_data.wlanacname = local_temp.portalForm.wlanacname;
-        post_data.wlanacIp = local_temp.serverForm.serverip;
-        post_data.vlan = local_temp.portalForm.vlan;
-        post_data.mac = local_temp.portalForm.mac;
-        post_data.version = local_temp.serverForm.portalVer;
-        post_data.portalpageid = local_temp.portalconfig.id;
-        post_data.timestamp = local_temp.portalconfig.timestamp;
-        post_data.uuid = local_temp.portalconfig.uuid;
 
 
-        //    off_line('172.16.253.121');
+            local_temp = JSON.parse(local_temp !== '' ? local_temp : '');
 
-    } else if (status_code === 200){
-        let url_ = ''
-        let bl = true
+            post_data.userid = my_server.account.name
+            post_data.passwd = my_server.account.password;
+            post_data.wlanuserip = local_temp.portalForm.wlanuserip;
+            post_data.wlanacname = local_temp.portalForm.wlanacname;
+            post_data.wlanacIp = local_temp.serverForm.serverip;
+            post_data.vlan = local_temp.portalForm.vlan;
+            post_data.mac = '';
+            post_data.version = local_temp.serverForm.portalVer;
+            post_data.portalpageid = local_temp.portalconfig.id;
+            post_data.timestamp = local_temp.portalconfig.timestamp;
+            post_data.uuid = local_temp.portalconfig.uuid;
+
+
+        }
+        if (login) {
+            console.log(`>> You have already login! [code: ${status_code}]`)
+            http.get("http://localhost:3000/login-finish")
+            waiting = false;
+            const NOTIFICATION_TITLE = '消息'
+            const NOTIFICATION_BODY = '您已成功登录'
+            new Notification({
+                title: NOTIFICATION_TITLE,
+                body:NOTIFICATION_BODY,
+                icon:iconPath
+            }).show()
+            return
+        }
+
+    }
+
+    let url_ = ''
+    let bl = true
+
+    if(status_code === 302) {
+        url_ = loc
+    } else {
+
         data.split('script').forEach(line => {
             line.split('\"').forEach(line => {
                 if (line.startsWith('http') && bl) {
@@ -110,14 +135,16 @@ async function handle_request(status_code, data){
                 }
             });
         })
+
+
         let end = url_.indexOf('&url=')
-        url_ = url_.substring(0,end)
-        const host = url_.substring(7,21)
-        console.log(`>> Listened url: ${url_}`)
+        url_ = url_.substring(0, end)
 
+    }
+    console.log(`>> Listened url: ${url_}`)
+    const host = url_.substring(7, 21)
 
-
-        const request = await http.request(url_,(res) => {
+    const request =  http.request(url_,(res) => {
             let data = ''
 
             res.on('data', (chunk) => {
@@ -128,15 +155,14 @@ async function handle_request(status_code, data){
             })
             res.on('end', () => {
                 console.log('>> Home page loaded!');
-                handle_post_data(host, url_.substring(38))
+                handle_post_data(host, url_.substring(38), login)
             })
         })
         request.end()
 
-    }
 }
 
-async function handle_post_data(host, url){   //处理请求文件\
+async function handle_post_data(host, url, login){   //处理请求文件
     let incoming_data = ''
 
     const option = {
@@ -173,25 +199,28 @@ async function handle_post_data(host, url){   //处理请求文件\
 
 
     if(incoming_data){
-        final_login(host, incoming_data, url)
+        final_process(host, incoming_data, url, login)
+
+    } else {
+        console.log(">> Get certificate data fail! request out")
+        waiting = false;
     }
 
 }
 
 
 
-async function final_login(host, incoming_data, url){
+async function final_process(host, incoming_data, url, login){
     let confirm_data = ''
     let redirect_url
     //灌注数据
-    //测试用
-    post_data.userid = '25122021076';
-    post_data.passwd = '17881952248'
+    post_data.userid = my_server.account.name;
+    post_data.passwd = my_server.account.password;
     post_data.wlanuserip = incoming_data.portalForm.wlanuserip
     post_data.wlanacname = incoming_data.portalForm.wlanacname;
     post_data.wlanacIp = incoming_data.serverForm.serverip;
     post_data.vlan = incoming_data.portalForm.vlan;
-    post_data.mac = incoming_data.portalForm.mac;
+    post_data.mac = url.substring(url.indexOf("mac=") + "mac".length, url.indexOf('&vlan'));
     post_data.version = incoming_data.serverForm.portalVer;
     post_data.portalpageid = incoming_data.portalconfig.id;
     post_data.timestamp = incoming_data.portalconfig.timestamp;
@@ -201,7 +230,7 @@ async function final_login(host, incoming_data, url){
 
     redirect_url = new URLSearchParams(post_data).toString();
     redirect_url += '&viewStatus=1';
-    console.log(`>> Making url for login... [url:${redirect_url}]`)   //超级拼装url
+    console.log(`>> Making url for param... [url:${redirect_url}]`)   //超级拼装url
 
     const option = {
         host: host,
@@ -230,38 +259,66 @@ async function final_login(host, incoming_data, url){
         })
         final_res.end();
     })}
+    if(login) {
 
-    confirm_data = await final_req();
-    confirm_data = JSON.parse(confirm_data);
+        confirm_data = await final_req();
+        confirm_data = JSON.parse(confirm_data);
 
-    if (confirm_data['code'] === '0'){
-        console.log(`>> Login succeed! code:${confirm_data['code']}`);
-    } else if (confirm_data['code'] === '1'){
-        console.error(`>> Login failed! Please check your username or password or report it to developer [message:${confirm_data["message"]}]`)
+        if (confirm_data['code'] === '0') {
+            console.log(`>> Login succeed! [code:${confirm_data['code']}]`);
+            http.get("http://localhost:3000/login-finish");
+            waiting = false;
+            const NOTIFICATION_TITLE = '消息'
+            const NOTIFICATION_BODY = '登录成功！'
+            new Notification({
+                title: NOTIFICATION_TITLE,
+                body:NOTIFICATION_BODY,
+                icon:iconPath
+            }).show()
+
+        } else if (confirm_data['code'] === '1') {
+            console.error(`>> Login failed! Please check your username or password or report it to developer [message:${confirm_data["message"]}]`)
+            http.get("http://localhost:3000/login-finish");
+            waiting = false;
+            const NOTIFICATION_TITLE = '消息'
+            const NOTIFICATION_BODY = '登录失败，请检查网络连接情况并核对账号密码是否正确'
+            new Notification({
+                title: NOTIFICATION_TITLE,
+                body:NOTIFICATION_BODY,
+                icon:iconPath
+            }).show()
+
+        }
+    } else {
+        off_line(host,redirect_url)
     }
 
 
 }
 
 
-async function off_line(host){
+async function off_line(host, param){
     let confirm_data = ''
-    let offline_post_data = new URLSearchParams({
+    let offline_post_data = {
         "wlanacip": post_data.wlanacIp,
         "wlanuserip": post_data.wlanuserip,
         "wlanacname": post_data.wlanacname,
         "version": post_data.version,
         "portaltype": post_data.portaltype,
         "userid": post_data.userid,
-        "mac": post_data.mac,
+        "mac": post_data.mac ? post_data.mac : '',
         "groupId": '',
-        "clearOperator": '1'
-    })
-    console.log(`>> Making url for offline: ${offline_post_data.toString()}`)
+        "clearOperator": '0'
+    }
     let option = {
         host:host,
-        path: '/quickauthdisconn.do?' + offline_post_data.toString(),
-        method:'GET',
+        path: '/quickauthdisconn.do?' + param,
+        method:'POST',
+        headers:{
+            'Content-Length': (JSON.stringify(offline_post_data)).length,
+            'Content-Type': 'application/json'
+
+        }
     }
 
 
@@ -279,10 +336,15 @@ async function off_line(host){
                     reject(err)
                 })
             })
-            logout_res.end();
+
             logout_res.on('error', (err) => {
                 console.error(`>> Logout failed! [CONNECTION]${err}`)
+                http.get("http://localhost:3000/logout-finish");
+                waiting = false;
             })
+            logout_res.write(JSON.stringify(offline_post_data));
+            logout_res.end();
+
         })
     }
     console.log('>> Trying to offline...')
@@ -290,13 +352,36 @@ async function off_line(host){
     confirm_data = JSON.parse(confirm_data);
 
     if(confirm_data['code'] === '0'){
-        console.log(`>> Logout succeed! code:${ confirm_data['code']}`);
+        console.log(`>> Logout succeed! [code:${ confirm_data['code']}]`);
+        http.get("http://localhost:3000/logout-finish");
+        waiting = false;
+        const NOTIFICATION_TITLE = '消息'
+        const NOTIFICATION_BODY = '下线成功！'
+        new Notification({
+            title: NOTIFICATION_TITLE,
+            body:NOTIFICATION_BODY,
+            icon:iconPath
+        }).show()
+    } else if (confirm_data['code'] === '1'){
+        console.error(`>> Logout failed! [code:${confirm_data['code']}]`)
+        http.get("http://localhost:3000/logout-finish");
+        waiting = false;
+        const NOTIFICATION_TITLE = '消息'
+        const NOTIFICATION_BODY = '下线失败！请检查账号密码是否正确'
+        new Notification({
+            title: NOTIFICATION_TITLE,
+            body:NOTIFICATION_BODY,
+            icon:iconPath
+        }).show()
     }
+
+
 
 }
 
 
 function make_form(){    //程序主界面
+
     let mainWindows;
     let quit = false;
     app.whenReady().then(() => {
@@ -304,10 +389,22 @@ function make_form(){    //程序主界面
             width:800,
             height:600,
             show: false,
-            icon: path.join(__dirname,'icon.png')
+            icon: path.join(__dirname,'icon.png'),
+            webPreferences : {    //网络安全问题导致这玩意不能自主发送请求
+                webSecurity:false,
+                contextIsolation:false
+            }
         })
 
-        mainWindows.loadURL('http://localhost:3000')
+        /*
+        session.defaultSession.clearStorageData({
+            storages: ['localstorage']
+        })
+
+         */
+
+        let host = my_server.host()
+        mainWindows.loadURL(`http://${host}:3000`)
 
         let tray= new Tray(iconPath);
 
@@ -349,25 +446,73 @@ function make_form(){    //程序主界面
 
     })
 
-    app.on('before-quit', (event) => {
+    app.on('before-quit', () => {
         console.log('>> program quit.')
         quit = true
+        //my_server.close_server();
+        if (scheduleInterval) clearInterval(scheduleInterval);
     })
+
+    app.on("quit",() =>{
+        my_server.close_server();
+    });
 
 }
 
 
-function start() {
+function on_schedule(){
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    if(my_server.time_schedule.plan[time_index].time === formattedTime){
+
+        console.log(`>> On schedule: ${formattedTime}, method:${my_server.time_schedule.plan[time_index].node}`)
+        make_request(() => {
+            if(my_server.time_schedule.plan[time_index].node === 'online'){
+                return true;
+            } else if (my_server.time_schedule.plan[time_index].node === 'offline'){
+                return false;
+            }
+        })
+    }
+
+}
+
+async function start() {
     console.log('>> Starting program...');
-    const my_server = new server();
-    console.log('>> Main server started')
-    my_server.start_server();
+    console.log('>> Main server started');
+    await my_server.start_server();
     make_form()
     setTimeout(() => {
-        make_request()
+        make_request(true)
     }, 2000)
 
 
-}
 
-start()
+    scheduleInterval = setInterval(() => {
+        function listen() {
+            if(!waiting) {
+                on_schedule()
+                if (my_server.is_login) {
+                    make_request(true)
+                } else if (my_server.is_logout) {
+                    make_request(false)
+                }
+
+            } else {
+            }
+        }
+        listen()
+
+    }, 1000)
+
+
+}
+const my_server = new server();
+
+start();
--- nodeJS_ver/server/server.js
+++ nodeJS_ver/server/server.js
@@ -1,37 +1,99 @@
 const express = require('express');
 const fs = require('fs');
 const path = require("path");
+const os = require('os')
+const chalk = require('chalk')
 
 class server {
     constructor() {
         this.name = 'server';
+        this.on_listen = undefined;
         this.port = 3000;
-        this.host = '127.0.0.1';
+        this.time_schedule = {
+            "plan":[
+                {
+                "time": "08:00",
+                    "node": "none"
+                }
+            ]
+        };
+        this.account = {
+            "name":"",
+            "password": ""
+        };
+        this.host = () => {
+            const interfaces =  os.networkInterfaces();
+            for(const name of Object.keys(interfaces)){
+                for (const iface of interfaces[name]){
+                    if(iface.family === 'IPv4' && !iface.internal){
+                        return iface.address;
+                    }
+                }
+            }
+            return "127.0.0.1"
+        };
         this.app = express();
+
+        this.is_login = false;
+        this.is_logout = false;
+        this.app.use(express.json())    //使用json作为返回一定要加上！！！！
+
 
         this.app.get( '/', (req, res) => {
             res.sendFile(path.join(__dirname, 'index.html'))
         });
 
+        this.app.post('/PushTimeData', (req, res) => {
+            console.log(">> [server] receive time plan ")
+            this.time_schedule = req.body;
+            console.log(req.body);
+        })
+
+        this.app.post('/PushAccount', (req, res) => {
+            console.log(">> [server] receive account ");
+            this.account = req.body;
+            console.log(req.body)
+        })
+        this.app.get('/login',(req,res) => {
+            console.log(">> [server] receive login")
+            this.is_login = true;
+            res.send("success")
+
+        })
+        this.app.get('/login-finish',(req,res) => {
+            console.log(">> [server][login] finish request,calling back...")
+            this.is_login = false;
+
+        })
+        this.app.get('/logout',(req,res) => {
+            console.log(">> [server] receive logout")
+            this.is_logout = true;
+            res.send("success")
+
+        })
+        this.app.get('/logout-finish',(req,res) => {
+            console.log(">> [server][logout] finish request,calling back...")
+            this.is_logout = false;
+        })
+
     }
 
     start_server() {
-        this.app.listen(this.port, this.host,() => {
+        this.on_listen = this.app.listen(this.port, this.host,() => {
             console.log('>> Server is running on port 3000');
         });
 
     }
 
     close_server(){
-        this.app().close(() => {
+        this.on_listen.close(() => {
             console.log('>> Server is closed');
+
         })
-    }
-
-
-    get_data(){
 
     }
+
+
 
 }
 
